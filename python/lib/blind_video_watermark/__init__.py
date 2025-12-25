e8d5383b166136c3febca669d2d2d4392beba6d9
exedesign <exedesign@github.com>
2025-12-25 19:33:45 +0300

    Remove large python/lib from git and add to .gitignore

--- python/lib/blind_video_watermark/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dtcwt_img import DtcwtImgEncoder, DtcwtImgDecoder
-from .dtcwt_key import DtcwtKeyEncoder, DtcwtKeyDecoder
--- python/lib/dtcwt/defaults.py
+++
@@ -1,3 +0,0 @@
-DEFAULT_BIORT='near_sym_a'
-
-DEFAULT_QSHIFT='qshift_a'
